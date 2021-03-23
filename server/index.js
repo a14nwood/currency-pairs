a68e9ea910988152a2d98eb46286e0c52e9c999b
Michael Wood <58078180+edohoim@users.noreply.github.com>
2021-03-23 16:21:48 -0700

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'This is the message!' });
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
