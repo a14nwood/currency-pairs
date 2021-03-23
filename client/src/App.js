858b5188011d26e4bc8a48c987e200ee7be00158
Michael Wood <58078180+edohoim@users.noreply.github.com>
2021-03-23 16:27:48 -0700

    Initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/test')
-      .then(res => res.json())
-      .then(data => setData(data.message));
-  }, []);
-
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {data ? data : 'Loading...'}
+        In development...
       </header>
     </div>
   );
 }
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,6 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.json({ message: 'This is the message!' });
-})
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
