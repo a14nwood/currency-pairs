c3138fb667666e1cfed651982b2660e7b4940be8
Michael Wood <58078180+edohoim@users.noreply.github.com>
2021-03-23 19:59:17 -0700

    Implement 24h ETH/USD price retrieval

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
+const { DateTime } = require('luxon');
+const { exchange, timeseries } = require('@sushiswap/sushi-data');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get('/pair:id', (req, res) => {
+  const oneDayAgo = DateTime.now().minus({ day: 1 }).startOf('hour');
+  const pairPromises = [];
+  const pairAddress = '0x397ff1542f962076d0bfe58ea045ffa2d347aca0';
+  for (let hours = 0; hours <= 24; ++hours) {
+    const timestamp = oneDayAgo.plus({ hour: hours }).toSeconds();
+    pairPromises.push(exchange.pair({ pair_address: pairAddress, timestamp }));
+  }
+
+  Promise.all(pairPromises)
+    .then(pairs => {
+      res.status(200).json(pairs.map(pair => pair.token0Price.toFixed(2)));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).json(err);
+    });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
